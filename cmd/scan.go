c16a363bbb6956c097572e885f2458616b172226
shit114-glitche <hayrieken@leerling.dbhaacht.be>
2025-06-21 21:57:42 +0200

    Update scan.go

--- cmd/scan.go
+++ cmd/scan.go
@@ -14,7 +14,7 @@ import (
 )
 
 type ScanCmdOptions struct {
-	Number           string
+	Number            +32487297350
 	DisabledScanners []string
 	PluginPaths      []string
 	EnvFiles         []string
